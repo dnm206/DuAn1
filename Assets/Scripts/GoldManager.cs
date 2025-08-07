428d1c06752892cc640715c0795c983865e60b5e
huulan2005 <huulanu100@gmail.com>
2025-08-07 12:53:12 +0700

    Acher Prefab
    .

---
+++ Assets/Scripts/GoldManager.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldManager : MonoBehaviour
+{
+    public static GoldManager Instance;
+
+    public int currentGold = 0;
+    public Text goldText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddGold(int amount)
+    {
+        currentGold += amount;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (goldText != null)
+        {
+            goldText.text = currentGold.ToString();
+        }
+    }
+}

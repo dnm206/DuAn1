9186ad808cf462c734def07ee06d8cfc9674860d
Ducky3103 <trinhducnbnv123@gmail.com>
2025-08-08 17:32:17 +0700

    health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float maxHealth = 100f; // Máu tối đa
+    private float currentHealth;
+
+    public Image healthBarFill; // Phần fill của thanh máu (UI Image)
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0) currentHealth = 0;
+
+        UpdateHealthBar();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthBarFill != null)
+        {
+            healthBarFill.fillAmount = currentHealth / maxHealth;
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject); // Xóa quái khi chết
+    }
+}
